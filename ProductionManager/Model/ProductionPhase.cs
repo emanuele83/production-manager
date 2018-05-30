63d4982a614343b59737771503b1b3d1e55facaa
emanuele <emanuele.sabbadini@gmail.com>
2018-05-30 23:21:57 +0200

    first advancement

---
+++ ProductionManager/Model/ProductionPhase.cs
@@ -0,0 +1,32 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionManager.Model
+{
+    public class ProductionPhase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+    public class ProductionPhaseModel
+    {
+        private LiteDatabase db;
+        private LiteCollection<ProductionPhase> collection;
+
+        public ProductionPhaseModel(LiteDatabase liteDatabase)
+        {
+            this.db = liteDatabase ?? throw new ArgumentNullException(nameof(liteDatabase));
+
+            collection = db.GetCollection<ProductionPhase>("phases");
+        }
+
+        public int AddProductionPhase(string name) => collection.Insert(new ProductionPhase() { Name = name });
+        public ProductionPhase GetProductionPhase(int id) => collection.FindById(id);
+        public IEnumerable<ProductionPhase> ProductionPhases => collection.FindAll();
+    }
+}
